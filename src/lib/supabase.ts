d56c9a94e8942d43f4819ffdb63bb47e6c99bc25
Your Name <you@example.com>
2026-01-09 20:07:28 +0100

    Use env vars for Supabase config

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://purbzxxjgdumubhbjahf.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1cmJ6eHhqZ2R1bXViaGJqYWhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUwNjI0NzcsImV4cCI6MjA1MDYzODQ3N30.Ej8Ej8Ej8Ej8'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
